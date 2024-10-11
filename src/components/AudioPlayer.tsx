df864beff9b40e8dd5eb57b6841d8ab9b014cb20
Subx1s0o <nazarsabardin@gmail.com>
2024-10-11 15:39:39 +0300

    add touch event listener

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -59,6 +59,7 @@ export default function AudioPlayer() {
         width="0"
         height="0"
         volume={volume}
+        
       />
       <ProgressBar
         duration={duration}
--- src/components/SongListItem.tsx
+++ src/components/SongListItem.tsx
@@ -18,6 +18,7 @@ const SongListItem = ({ song, index }: Props) => {
   return (
     <li
       onClick={() => selectSong(index)}
+      onTouchEnd={() => selectSong(index)}
       className="cursor-pointer border rounded-xl px-4 py-2 flex justify-between items-center sm:hover:text-white sm:hover:bg-black transition-colors relative"
     >
       <div className="flex flex-col gap-1">
